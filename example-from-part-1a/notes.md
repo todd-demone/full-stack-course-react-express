485c299d123c16dd2bc7116cf7007588ed14ef9a
Todd Demone <tdemone1@gmail.com>
2021-01-10 12:17:10 -0500

    completed 1.3 to 1.5

--- example-from-part-1a/notes.md
+++ example-from-part-1a/notes.md
@@ -1,4 +1,4 @@
-# Part 1a - React intro
+# Part 1a - Fundamentals of React
 
 ## ReactDOM.render()
 argument 1- takes a Component
@@ -32,3 +32,54 @@ argument 2 - tells it where in DOM to render that component
 * if value is result of JS expn, it must be written in curly braces
 * e.g., `const name="Peter"; return (<Hello name={name}>)`
 
+
+# JavaScript section
+
+const - can't change value unless variable is pointing to an object (eg array), then can change individual values of object
+let
+arrays
+arr.push(val)
+arr.forEach(val => { // function is called once for each value in arr })
+
+react - use functional techniques
+- keep data immutable
+- t/f `concat` instead of `push` - create new array instead of modifying old array
+```
+arr = [1, 2, 3]
+newArr = arr.concat(5)
+console.log(arr)
+console.log(newArr)
+```
+### `map` method for arrays
+* used often in react
+```
+const t = [1, 2, 3]
+const m1 = t.map(value => value * 2)
+console.log(m1) // [2, 4, 6]
+
+const m2 = map(value => '<li>' + value + '</li>') // transformed from integers into html
+```
+
+### Objects
+* object literals 
+const object1 = {
+    name: 'Arto',
+    age: 35,
+    education: 'PhD'
+}
+
+getting and setting object properties
+```
+console.log(object1.name)
+object1.address = 'Helsinki'
+object1['secret number'] = 12341
+```
+* object can have methods
+* objects can be defined using constructor functions
+* also using ES^ class syntax
+
+### Functions
+
+arrow functions
+- can cut corners on parentheses if single parameter
+- can remove curly braces and return statement if just single expression
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  // ex 1.5 - using a single object for entire course
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  // // ex 1.3 - Using object literals
+  // const part1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const part2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const part3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
+
+  // // ex 1.4 - Using and array of objects
+  // const course = 'Half Stack application development'
+  // const parts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name: 'State of a component',
+  //     exercises: 14
+  //   }
+  // ] 
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+ return (
+  <h1>{props.course}</h1>
+ )
+}
+
+const Content = (props) => {
+ return (
+  <>
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+  </>
+ )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[1].exercises}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
