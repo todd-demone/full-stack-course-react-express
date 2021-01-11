5f87630f1dca26de833dd7d8b0fea8d189292968
Todd Demone <tdemone1@gmail.com>
2021-01-11 14:31:44 -0500

    completed ex 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({text, value}) => (
+  <p>{text} {value}</p>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h2>statistics</h2>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
