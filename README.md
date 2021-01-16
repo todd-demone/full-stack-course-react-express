0f472fb05a8adad5ad82500239609eddd4702cae
Todd Demone <tdemone1@gmail.com>
2021-01-16 07:56:13 -0500

    working on ex. 2.11

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Full Stack Open 2020
 
-My solutions to the exercises from the *2020 Full Stack Open* course offered by the University of Helsinki.
+My solutions to the exercises from the [2020 Full Stack Open](https://fullstackopen.com/en/) course offered by the University of Helsinki.
 
---
+++ part2/example-part-2/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// import axios from 'axios'
+
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     ReactDOM.render(
+//       <App notes={notes} />,
+//       document.getElementById('root')
+//     )  
+//   })
+
+// const mypromise2 = axios.get('http://localhost:3001/foobar')
+// console.log(mypromise2)
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2020-01-10T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only Javascript',
+//     date: '2020-01-10T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2020-01-10T19:20:14.298Z',
+//     important: true
+//   }
+// ]
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
